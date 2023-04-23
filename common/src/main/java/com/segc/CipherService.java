545402808a07faed55155de71cb9132d5aa4bef5
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2023-04-23 15:57:36 +0100

    Make CipherService and AuthenticationService non-singleton.

--- common/src/main/java/com/segc/CipherService.java
+++ common/src/main/java/com/segc/CipherService.java
@@ -1,30 +1,47 @@
 package com.segc;
 
 import java.io.*;
-import java.security.KeyStore;
+import java.security.*;
+import java.security.cert.Certificate;
 
+/**
+ * A class that handles encryption and decryption.
+ *
+ * @author fc54685 Francisco Correia
+ * @author fc55955 Alexandre Fonseca
+ * @author fc56272 Filipe Egipto
+ */
 public class CipherService {
-    private static final Configuration config = Configuration.getInstance();
-    private static CipherService instance;
-    private static KeyStore ks;
 
-    private CipherService() {
-        instance = new CipherService();
+    private final KeyStore ks;
 
-        ks = initKeyStore(config.getValue("keyStoreFile"),
-                config.getValue("keyStorePassword"),
-                config.getValue("keyStoreFormat"));
+    public CipherService(File keyStoreFile, char[] keyStorePassword, String keyStoreFormat) {
+        ks = initKeyStore(keyStoreFile, keyStorePassword, keyStoreFormat);
     }
 
-    public static CipherService getInstance() {
-        return instance;
+    public Key getKey(String alias, char[] password) throws UnrecoverableKeyException {
+        try {
+            return ks.getKey(alias, password);
+        } catch (KeyStoreException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private KeyStore initKeyStore(String keyStoreFile, String keyStorePassword, String keyStoreFormat) {
+    public Certificate getCertificate(String alias) {
+        try {
+            return ks.getCertificate(alias);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private KeyStore initKeyStore(File keyStoreFile, char[] keyStorePassword, String keyStoreFormat) {
         try (FileInputStream kfile = new FileInputStream(keyStoreFile)) {
             KeyStore ks = KeyStore.getInstance(keyStoreFormat);
-            ks.load(kfile, keyStorePassword.toCharArray());
+            ks.load(kfile, keyStorePassword);
             return ks;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("You must generate a keystore before running this application.", e);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
